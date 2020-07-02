ed116fec9b55d5614925d7e8daf96feb61325338
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-02 15:28:27 -0400

    Added validation logic for the fake users.

--- shared/Api.fs
+++ shared/Api.fs
@@ -14,6 +14,24 @@ module AlphaNumericCode =
         UserPrefix: string
         UniversalNumber: int
     }
+
+    /// A validation function based on length
+    let private lengthValidator len (s:string) =
+        s.Length <= len
+
+    let private userPrefixValidator userPrefix userRole =
+        match userRole with
+        | Student when lengthValidator 3 userPrefix && userPrefix = "STU"-> true
+        | Professor when lengthValidator 4 userPrefix && userPrefix = "PROF"-> true
+        | _ -> false
+
+    let create userPrefix universalNumber userRole =
+        let isUserPrefixValid = userPrefixValidator userPrefix userRole
+        let isUniversalNumberValid = lengthValidator 5 universalNumber
+        match (isNull userPrefix, isNull universalNumber, isUserPrefixValid, isUniversalNumberValid)  with
+        | (false, false, true, true) -> Some { UserPrefix = userPrefix; UniversalNumber = universalNumber |> int }
+        | _ -> None
+
 /// A type that specifies the communication protocol between client and server
 /// to learn more, read the docs at https://zaid-ajaj.github.io/Fable.Remoting/src/basics.html
 type IServerApi = {
