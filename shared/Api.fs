2552973476598b42e0efb6577740cc27f1dc6104
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-02 15:31:51 -0400

    Added API endpoint to log a user.

--- shared/Api.fs
+++ shared/Api.fs
@@ -72,5 +72,5 @@ type ValidatedUser =
 /// A type that specifies the communication protocol between client and server
 /// to learn more, read the docs at https://zaid-ajaj.github.io/Fable.Remoting/src/basics.html
 type IServerApi = {
-    Counter : unit -> Async<Counter>
+    SignIn: MockUserCredentials -> Result<ValidatedUser, string>
 }
