bdcdc9a02361f54a2b0f769b41013a20cdd4469e
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:14:59 -0400

    Added implemented for a valid user.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/ValidUsers/ValidUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Data.Helpers;
+using BlazorApp1.Data.School;
+
+namespace BlazorApp1.Data.ValidUsers
+{
+    public abstract class ValidUser
+    {
+        protected ValidUser(CommonUserInformation userInformation)
+        {
+            this.UserInformation = userInformation ?? throw new ArgumentNullException(nameof(userInformation));
+        }
+
+        public CommonUserInformation UserInformation { get; }
+    }
+}
