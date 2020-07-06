72c4ebceb1da0a09c915ccea2671bae51e6b656a
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:12:47 -0400

    Implemented the type to represent a course.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/School/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Data.ValidUsers;
+
+namespace BlazorApp1.Data.School
+{
+    public class Course
+    {
+        public string Name { get; }
+
+        public WeeklySchedule Hours { get; }
+
+        public Assessment[] Evaluations { get; }
+
+        public int StudentMaxCapacity { get; }
+
+        public Professor Instructor { get; }
+
+        public string Location { get; }
+
+        public string[] Objectives { get; }
+
+        public string RecommendedReadings { get; }
+
+        public string Code { get; }
+
+        public string? PreRequisites { get; }
+    }
+}
