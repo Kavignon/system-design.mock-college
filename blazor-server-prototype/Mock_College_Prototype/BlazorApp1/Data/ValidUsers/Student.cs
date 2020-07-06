eca7836a1a06c48c297cb699f36b4aee9f49d2c3
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:15:07 -0400

    Implemented the student valid user.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/ValidUsers/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Data.School;
+
+namespace BlazorApp1.Data.ValidUsers
+{
+    public sealed class Student : ValidUser
+    {
+        public Student(CommonUserInformation userInformation, Gpa gpa, int completedCredits, int enrolledCredits) 
+            : base(userInformation)
+        {
+            this.GPA = gpa;
+            this.CompletedCreditCount = completedCredits;
+            this.EnrolledCreditCount = enrolledCredits;
+        }
+
+        public Gpa GPA { get; }
+
+        public int CompletedCreditCount { get; }
+
+        public int EnrolledCreditCount { get; }
+    }
+}
