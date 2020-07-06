6466f585b86d787cc272f8aabd77ce3428fde560
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:14:45 -0400

    Implemented full name to capture the name of a user.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/ValidUsers/FullName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Data.Helpers;
+
+namespace BlazorApp1.Data.ValidUsers
+{
+    public class FullName
+    {
+        public FullName(string firstName, string? middleName, string lastName) =>
+            this.Value = FullNameCreator.GenerateFullName(firstName, middleName, lastName);
+
+        public string Value { get; }
+    }
+}
