9bd8814af26a3c5cf79515e6a8c4720c04c8ee93
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:15:18 -0400

    Implemented the professor valid user.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/ValidUsers/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data.ValidUsers
+{
+    public sealed class Professor : ValidUser
+    {
+        public Professor(CommonUserInformation userInformation, int supervisedStudentCount) 
+            : base(userInformation)
+        {
+            this.SupervisedStudentCountForSemester = supervisedStudentCount;
+        }
+
+        public int SupervisedStudentCountForSemester { get; }
+    }
+}
