ff0eabf92368fc4b85574dcc5e5880d5a1f74a5e
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:11:31 -0400

    Added helper to generate full name of valid user.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/Helpers/FullNameCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data.Helpers
+{
+    public static class FullNameCreator
+    {
+        public static string GenerateFullName(string firstName, string? middleName, string lastName)
+        {
+            return (firstName, middleName, lastName) switch
+            {
+                var (missing, _, _) when string.IsNullOrEmpty(missing) => throw new ArgumentNullException(nameof(firstName)),
+                var (_, _, missing) when string.IsNullOrEmpty(missing) => throw new ArgumentNullException(nameof(lastName)),
+                var (f, m, l) when f.Length >= 1 && l.Length >= 1 => $"{f} {m} {l}"
+            };
+        }
+
+    }
+}
