88f1dd8e0d854431dd8406849a6a6da5de5da477
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:12:14 -0400

    Added helper to created the GPA of a student.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/Helpers/GpaCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Data.School;
+
+namespace BlazorApp1.Data.Helpers
+{
+    public class GpaCreator
+    {
+        public static Gpa MakeAverageGpaFromPastResult(IReadOnlyCollection<float> studentPastGrades)
+        {
+            return new Gpa(studentPastGrades.Average());
+        }
+    }
+}
