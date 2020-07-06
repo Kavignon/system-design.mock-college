5c3c271fd89ca015ade01230f45892a19e01743f
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-07-06 13:12:02 -0400

    Implemented the GPA type to capture the GPA of a student.

---
+++ blazor-server-prototype/Mock_College_Prototype/BlazorApp1/Data/School/Gpa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data.School
+{
+    public class Gpa
+    {
+        public Gpa(float averageValue)
+        {
+            this.Value = averageValue;
+        }
+
+        public float Value { get; }
+    }
+}
